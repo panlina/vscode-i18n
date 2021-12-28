ede29172a4eed19e0eb56f46b7dff39e71d68877
Jack Lu <jacklu@jacklu.me>
2021-12-28 22:49:03 +0800

    migrate reference provider

---
+++ src/ReferenceProvider.ts
@@ -0,0 +1,40 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as babel from '@babel/core';
+var FILE = require(path.join(vscode.workspace.rootPath!, 'node_modules', 'babel-plugin-i18n/FILE'));
+import ast from './ast';
+
+export function activateReferenceProvider(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.languages.registerReferenceProvider(["javascript", "javascriptreact", "typescript", "typescriptreact"], new ReferenceProvider())
+	);
+};
+
+class ReferenceProvider implements vscode.ReferenceProvider {
+	async provideReferences(document: vscode.TextDocument, position: vscode.Position, context: vscode.ReferenceContext, token: vscode.CancellationToken) {
+		let node: babel.Node[] = [];
+		babel.traverse(ast.get(document)!, {
+			enter(path) {
+				if ((path.node as any).$$i18n)
+					if (path.node.start! < document.offsetAt(position) && path.node.end! > document.offsetAt(position))
+						node.push(path.node);
+			}
+		});
+		return Promise.all(node.flatMap(node => {
+			var { source: source, target: target } = (node as any).$$i18n;
+			return Object.entries(target)
+				.filter(([language, target]) => target && typeof target != 'string')	// custom translator has `typeof target == 'string'`
+				.map(async ([language, target]) => {
+					var [, { [FILE]: dictionary }] = target as any;
+					var document = await vscode.workspace.openTextDocument(dictionary);
+					var offset = fs.readFileSync(dictionary, 'utf8').indexOf(JSON.stringify(source));
+					var position = document.positionAt(offset);
+					return new vscode.Location(
+						vscode.Uri.file(dictionary),
+						position
+					);
+				});
+		}));
+	}
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode';
 import { activateDiagnostic } from './diagnostic';
 import { activateTranslationGeneration } from './translationGeneration';
+import { activateReferenceProvider } from './ReferenceProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 	activateDiagnostic(context);
 	activateTranslationGeneration(context);
+	activateReferenceProvider(context);
 }
 
 export function deactivate() {}
