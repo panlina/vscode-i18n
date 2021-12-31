87675c802b420fa2b4fa4f04d4bea494710d2ab2
Jack Lu <jacklu@jacklu.me>
2021-12-31 17:22:02 +0800

    babel-plugin-i18n->@jacklu/babel-plugin-i18n

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VS Code extension for babel-plugin-i18n
+# VS Code extension for @jacklu/babel-plugin-i18n
 
 This extension integrates [babel-plugin-i18n](http://github.com/panlina/babel-plugin-i18n) into VS code, which highlights untranslated texts and helps you generate translations.
 
--- src/requireBabelPluginI18n.ts
+++ src/requireBabelPluginI18n.ts
@@ -2,5 +2,5 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 
 export default function requireBabelPluginI18n(_path: string) {
-	return require(path.join(vscode.workspace.rootPath!, 'node_modules', 'babel-plugin-i18n', _path));
+	return require(path.join(vscode.workspace.rootPath!, 'node_modules', '@jacklu/babel-plugin-i18n', _path));
 }
